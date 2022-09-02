17f4a3ff7a6627d62b6cf09bcbe3f1c6434709ea
Breno <eusoubrenovieira@gmail.com>
2022-09-02 20:49:36 -0300

    Finished challenge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,31 @@
 import AddressInfos from "@/components/AddressInfos"
 import Header from "@/components/Header"
 import MapNoSSR from "@/components/MapIndex"
-import type { GetServerSidePropsContext, NextPage } from "next"
+import type { NextPage } from "next"
 
-const Home: NextPage = (props:any) => {
+const Home: NextPage = (props: any) => {
 	return (
 		<>
 			<Header />
 			<AddressInfos ClientIp={props.ip} />
-			<MapNoSSR Ip={props.ip} />
+			<MapNoSSR Ip={props.ip}/>
 		</>
 	)
 }
 
 export default Home
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
+export async function getServerSideProps(context: any) {
 	let ip
 
 	const { req } = context
 
 	if (req.headers["x-forwarded-for"]) {
-		ip = req.headers.authorization?.split(",")[0]
+		ip = req.headers["x-forwarded-for"].split(",")[0]
 	} else if (req.headers["x-real-ip"]) {
-		ip = req.socket.remoteAddress
+		ip = req.connection.remoteAddress
 	} else {
-		ip = req.socket.remoteAddress
+		ip = req.connection.remoteAddress
 	}
 
 	return {
